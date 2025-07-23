265b51b7f16c8fe048bcb0aff7fad94bffe8eb62
MashaGoro21 <s27179@pjwstk.edu.pl>
2025-07-23 15:08:26 +0200

    First Person Starter Asset, Nav Mesh Agent, Equip a Gun

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,26 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    FirstPersonController player;
+
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+}
