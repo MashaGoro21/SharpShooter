7249bb63c7bd1cd2dc2ccffa0c94761f380489aa
MashaGoro21 <s27179@pjwstk.edu.pl>
2025-07-25 10:26:16 +0200

    Raycasting, Shoot Input, Enemy Health, Muzzle Flash

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+using StarterAssets;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] int damageAmount = 1;
+
+    StarterAssetsInputs starterAssetsInputs;
+
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+    }
+
+    private void Update()
+    {
+        HandleShoot();
+    }
+
+    private void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        {
+            EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+            enemyHealth?.TakeDamage(damageAmount);
+        }
+
+        starterAssetsInputs.ShootInput(false);
+    }
+}
