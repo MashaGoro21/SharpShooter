8510e47b6aebea36e7236d734fb849a4e304b63b
MashaGoro21 <s27179@pjwstk.edu.pl>
2025-07-31 14:25:58 +0200

    Robot Explosion VFX, Gizmos, Death Camera Transition

---
+++ Assets/Scripts/Enemies/Robot.cs
@@ -0,0 +1,37 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Robot : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent agent;
+
+    const string PLAYER_STRING = "Player";
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+    }
+
+    private void Update()
+    {
+        if (!player) return;
+        agent.SetDestination(player.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag(PLAYER_STRING))
+        {
+            EnemyHealth enemyHealth = GetComponent<EnemyHealth>();
+            enemyHealth.SelfDestruct();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int startingHealth = 5;
+    [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
+    [SerializeField] Transform weaponCamera;
+
+    int currentHealth;
+    int gameOverVirtualCameraPriority = 20;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log(amount + " damage taken");
+
+        if (currentHealth <= 0)
+        {
+            weaponCamera.parent = null;
+            deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
+            Destroy(this.gameObject);
+        }
+    }
+}
